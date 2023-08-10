b0622e721c11cc424b80632b6b2de3ae810f952d
Niklas Eberts <99niklas@gmx.de>
2023-08-10 19:15:48 +0200

    Start of the project Added parameters of PokeGenies csv file

---
+++ Compare.py
@@ -0,0 +1,12 @@
+
+
+class Pokemon:
+    def __init__(self, Index,Name,Form,Pokemon,Gender,CP,HP,AtkIV,DefIV,StaIV,IVAvg,LevelMin,LevelMax,QuickMove,ChargeMove,ChargeMove2,ScanDate,CatchDate,Weight,Height,Lucky,Shadow/Purified,Favorite,Dust,Rank % G,Rank Nr G,Stat Product G,Dust Cost G,Candy Cost G,Name G,Form G,Sha/Pur G,Rank % U,Rank Nr U,Stat Product U,Dust Cost U,Candy Cost U,Name U,Form U,Sha/Pur U,Rank % L,Rank Nr L,Stat Product L,Dust Cost L,Candy Cost L,Name L,Form L,Sha/Pur L,MarkedforPvPUse):
+        self.name = name
+        self.evolution = evolution
+        self.IV = IV
+        self.IVpvp = IVpvp
+        
+    
+if __name__ == "__main__":
+    print("Hello World")
