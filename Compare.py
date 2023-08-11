5cac0a3b1e4ae4393adfe8a844fcd7a73d704ae9
Niklas Eberts <99niklas@gmx.de>
2023-08-11 14:56:10 +0200

    Added sorting to the dataframe

--- Compare.py
+++ Compare.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-pokemonDataAll = pd.read_csv("data/poke_genie_export.csv")
+df = pd.read_csv("data/poke_genie_export.csv")
     
 if __name__ == "__main__":
-    print(pokemonDataAll)
+    dfTest = df.head(15)
+    dfTest.sort_values(by=['Name', 'CP'], ascending=[True, False])
+    print(dfTest.to_string())
