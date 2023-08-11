a4408ef73221a374e7f3f488a90d15eead2de726
Niklas Eberts <99niklas@gmx.de>
2023-08-11 12:13:20 +0200

    Removed the class and added the pandas library to handle the data

--- Compare.py
+++ Compare.py
@@ -1,12 +1,6 @@
+import pandas as pd
 
-
-class Pokemon:
-    def __init__(self, Index,Name,Form,Pokemon,Gender,CP,HP,AtkIV,DefIV,StaIV,IVAvg,LevelMin,LevelMax,QuickMove,ChargeMove,ChargeMove2,ScanDate,CatchDate,Weight,Height,Lucky,Shadow/Purified,Favorite,Dust,Rank % G,Rank Nr G,Stat Product G,Dust Cost G,Candy Cost G,Name G,Form G,Sha/Pur G,Rank % U,Rank Nr U,Stat Product U,Dust Cost U,Candy Cost U,Name U,Form U,Sha/Pur U,Rank % L,Rank Nr L,Stat Product L,Dust Cost L,Candy Cost L,Name L,Form L,Sha/Pur L,MarkedforPvPUse):
-        self.name = name
-        self.evolution = evolution
-        self.IV = IV
-        self.IVpvp = IVpvp
-        
+pokemonDataAll = pd.read_csv("data/poke_genie_export.csv")
     
 if __name__ == "__main__":
-    print("Hello World")
+    print(pokemonDataAll)
