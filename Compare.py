4d604e39ce24adb96d089819cb4b69cdbd32aabd
Niklas Eberts <99niklas@gmx.de>
2023-08-12 16:24:08 +0200

    Added a functionality to store all the evolutions in a file for a quick lookup and easy updates. Therefore, removed the need to send requests to the Pokemon API every lookup.

--- Compare.py
+++ Compare.py
@@ -5,32 +5,51 @@ import json
 df = pd.read_csv("data/poke_genie_export.csv")
 
 
-def onlyHighest(df):
-    return df.drop_duplicates(subset=['Name', 'Form'])
+def writeEvolutionsToFile():
+    evolutionList = createEvolutionList()
+    with open("data/evolutions.txt", "w") as filehandle:
+        json.dump(evolutionList, filehandle)
+
+
+def readEvolutionsFromFile():
+    evolutionListReadFromFile = []
+    with open("data/evolutions.txt", "r") as filehandle:
+        evolutionListReadFromFile = json.load(filehandle)
+    return evolutionListReadFromFile
+
+
+def onlyHighest(dataf):
+    return dataf.drop_duplicates(subset=['Name', 'Form'])
 
 
 def createEvolutionList():
     allEvolutions = []
-    for i in range(1, 539):
+    for i in range(1, 539):  # 538 is the last entry at the moment
+        print("Lookup for Number: " + str(i))
         evolution = []
 
         URL = "https://pokeapi.co/api/v2/evolution-chain/" + str(i) + "/"
         data = requests.get(url=URL)
-        json_object = json.loads(data.text)
 
-        firstStage = json_object['chain']['species']['name']
-        secondStage = json_object['chain']['evolves_to']
+        try:
+            json_object = json.loads(data.text)
 
-        evolution.append(firstStage)
-        for pokemon in secondStage:
-            evolution.append(pokemon['species']['name'])
+            firstStage = json_object['chain']['species']['name']
+            secondStage = json_object['chain']['evolves_to']
 
-            thirdStage = pokemon['evolves_to']
-            for pokemon2 in thirdStage:
-                evolution.append(pokemon2['species']['name'])
+            evolution.append(firstStage)
+            for pokemon in secondStage:
+                evolution.append(pokemon['species']['name'])
 
-        if evolution not in allEvolutions:
-            allEvolutions.append(evolution)
+                thirdStage = pokemon['evolves_to']
+                for pokemon2 in thirdStage:
+                    evolution.append(pokemon2['species']['name'])
+
+            if evolution not in allEvolutions:
+                allEvolutions.append(evolution)
+
+        except:
+            print("Number " + str(i) + " was not found")
 
     return allEvolutions
 
@@ -43,31 +62,8 @@ def dfTestingArea():
     print(onlyHighest(dfTest).to_string())
 
 
-def pokebaseTestingArea():
-    id = 538  # 538 is the last entry
-    URL = "https://pokeapi.co/api/v2/evolution-chain/" + str(id) + "/"
-    data = requests.get(url=URL)
-
-    # print(data.json())
-    # df = pd.DataFrame.from_dict(data.json())
-    # print(df.to_string())
-
-    json_object = json.loads(data.text)
-    # print(json.dumps(json_object, indent=2))
-
-    firstStage = json_object['chain']['species']['name']
-    secondStage = json_object['chain']['evolves_to']
-
-    print(firstStage)
-    for pokemon in secondStage:
-        print(pokemon['species']['name'])
-
-        thirdStage = pokemon['evolves_to']
-        for pokemon2 in thirdStage:
-            print(pokemon2['species']['name'])
-
-
 if __name__ == "__main__":
+    #writeEvolutionsToFile()
+    print(readEvolutionsFromFile()[100])
     # dfTestingArea()
-    #pokebaseTestingArea()
-    print(createEvolutionList())
+    # print(createEvolutionList())
