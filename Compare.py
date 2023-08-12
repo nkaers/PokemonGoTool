6d72c8c2ffe3284e73af171f4fcbc02eacc9dbda
Niklas Eberts <99niklas@gmx.de>
2023-08-12 12:02:19 +0200

    Added a function to filter the data to only get the highest CP Pokemon for each individual

--- Compare.py
+++ Compare.py
@@ -1,8 +1,14 @@
 import pandas as pd
 
 df = pd.read_csv("data/poke_genie_export.csv")
-    
+
+
+def onlyHighestCP(df):
+    return df.drop_duplicates(subset=['Name'])
+
+
 if __name__ == "__main__":
     dfTest = df.head(15)
-    dfTest.sort_values(by=['Name', 'CP'], ascending=[True, False])
-    print(dfTest.to_string())
+    dfTest = dfTest.sort_values(by=['Name', 'CP'], ascending=[True, False])
+    #print(dfTest.to_string())
+    print(onlyHighestCP(dfTest).to_string())
